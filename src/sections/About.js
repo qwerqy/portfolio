1870bf8c157312d1c1e1726d33037d453bf70e4d
Amin Roslan <amnrsln@gmail.com>
2020-05-26 19:28:43 +0800

    Add profile picture

--- src/sections/About.js
+++ src/sections/About.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Box, Flex } from 'rebass/styled-components';
+import { Box, Flex, Image } from 'rebass/styled-components';
 import { StaticQuery, graphql } from 'gatsby';
-// import styled from 'styled-components';
+import styled from 'styled-components';
 import ReactMarkdown from 'react-markdown';
 import Fade from 'react-reveal/Fade';
 import Section from '../components/Section';
@@ -32,14 +32,14 @@ import markdownRenderer from '../components/MarkdownRenderer';
 //   </div>
 // );
 
-// const ProfilePicture = styled(Image)`
-//   border-radius: 50%;
-//   transition: all 0.25s ease-out;
+const ProfilePicture = styled(Image)`
+  border-radius: 50%;
+  transition: all 0.25s ease-out;
 
-//   &:hover {
-//     border-radius: 20%;
-//   }
-// `;
+  &:hover {
+    border-radius: 20%;
+  }
+`;
 
 const About = () => (
   <Section.Container id="about">
@@ -63,10 +63,8 @@ const About = () => (
         }
       `}
       render={(data) => {
-        const {
-          aboutMe,
-          // profile
-        } = data.contentfulAbout;
+        const { aboutMe, profile } = data.contentfulAbout;
+
         return (
           <Flex justifyContent="center" alignItems="center" flexWrap="wrap">
             <Box width={[1, 1, 3 / 5]} px={[0, 0, 0]}>
@@ -82,14 +80,14 @@ const About = () => (
               width={[1, 1, 2 / 6]}
               style={{ maxWidth: '300px', margin: 'auto' }}
             >
-              {/* <Fade right>
+              <Fade right>
                 <ProfilePicture
                   src={profile.image.src}
                   alt={profile.title}
                   mt={[4, 4, 0]}
                   ml={[0, 0, 1]}
                 />
-              </Fade> */}
+              </Fade>
             </Box>
           </Flex>
         );
